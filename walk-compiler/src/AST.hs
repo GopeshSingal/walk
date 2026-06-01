01eb3682d8a59b5f082f3090bbe1299caea91a13
GopeshSingal <gopesh.singal.03@gmail.com>
2026-05-31 21:00:08 -0400

    feat: Expand AST.hs to support .str and .enm files

--- walk-compiler/src/AST.hs
+++ walk-compiler/src/AST.hs
@@ -1,15 +1,20 @@
 module AST
     ( WalkAST (..)
     , FunFile (..)
+    , StrFile (..)
+    , EnmFile (..)
     , ContractDecl (..)
+    , FieldDecl (..)
     , Stmt (..)
     , StringPart (..)
     ) where
 
 import Data.Text (Text)
 
-newtype WalkAST
+data WalkAST
     = FunAST FunFile
+    | StrAST StrFile
+    | EnmAST EnmFile
     deriving (Eq, Show)
 
 data FunFile = FunFile
@@ -19,9 +24,25 @@ data FunFile = FunFile
     }
     deriving (Eq, Show)
 
+data StrFile = StrFile
+    { strFields :: ![FieldDecl]
+    }
+    deriving (Eq, Show)
+
+data EnmFile = EnmFile
+    { enmVariants :: ![Text]
+    }
+    deriving (Eq, Show)
+
 data ContractDecl = ContractDecl
     { contractName :: !Text
     , contractType :: !Text
+    }
+    deriving (Eq, Show)
+
+data FieldDecl = FieldDecl
+    { fieldName :: !Text
+    , fieldType :: !Text
     }
     deriving (Eq, Show)
 
