6575f3babed76cdf0b20a42884b9f0d8ee231c7a
GopeshSingal <gopesh.singal.03@gmail.com>
2026-06-01 20:25:18 -0400

    feat: Parse through .str and .enm files

--- walk-compiler/src/Parser.hs
+++ walk-compiler/src/Parser.hs
@@ -3,16 +3,19 @@
 module Parser
     ( ParseError (..)
     , parseFunFile
+    , parseStrFile
+    , parseEnmFile
     , parseSnapshot)
     where
 import AST
 import Control.Concurrent.Async (mapConcurrently)
+import Data.Functor ((<&>))
 import Data.Map.Strict (Map)
 import Data.Text (Text)
 import Data.Void (Void)
 import Lexer
 import Snapshot (ExtensionKind (..), ProjectSnapshot (..), fileExtensionKind, snapshotFiles)
-import Text.Megaparsec (Parsec, eof, errorBundlePretty, many, runParser, try, optional)
+import Text.Megaparsec (Parsec, eof, errorBundlePretty, many, runParser, optional)
 import qualified Data.Map.Strict as Map
 
 data ParseError
@@ -22,11 +25,20 @@ data ParseError
 
 type P = Parsec Void Text
 
-parseFunFile :: FilePath -> Text -> Either ParseError FunFile
-parseFunFile path text =
-    case runParser (funFile <* eof) path text of
+runParse :: FilePath -> Text -> P a -> Either ParseError a
+runParse path text p =
+    case runParser (p <* eof) path text of
         Left bundle -> Left (ParseFailed path (errorBundlePretty bundle))
-        Right ast   -> Right ast
+        Right ast -> Right ast
+
+parseFunFile :: FilePath -> Text -> Either ParseError FunFile
+parseFunFile path text = runParse path text funFile
+
+parseStrFile :: FilePath -> Text -> Either ParseError StrFile
+parseStrFile path text = runParse path text strFile
+
+parseEnmFile :: FilePath -> Text -> Either ParseError EnmFile
+parseEnmFile path text = runParse path text enmFile
 
 parseSnapshot :: ProjectSnapshot -> IO (Either ParseError (Map FilePath WalkAST))
 parseSnapshot snap =
@@ -44,9 +56,11 @@ parseEntry (path, text) =
     pure $
         case fileExtensionKind path of
             Just Fun ->
-                case parseFunFile path text of
-                    Left err -> Left err
-                    Right parsed -> Right (path, FunAST parsed)
+                FunAST <$> parseFunFile path text <&> (path,)
+            Just Str ->
+                StrAST <$> parseStrFile path text <&> (path,)
+            Just Enm ->
+                EnmAST <$> parseEnmFile path text <&> (path,)
             Just kind ->
                 Left (UnsupportedExtension path kind)
             Nothing ->
@@ -64,6 +78,25 @@ funFile = sc *> do
     outs <- many (optional sc *> contractOut)
     stmts <- many (optional sc *> stmt)
     pure (FunFile ins outs stmts)
+
+strFile :: P StrFile
+strFile = sc *> do
+    fields <- many (optional sc *> fieldDecl)
+    pure (StrFile fields)
+
+fieldDecl :: P FieldDecl
+fieldDecl = do
+    name <- identifierText
+    _    <- symbol ":"
+    FieldDecl name <$> typeName
+
+enmFile :: P EnmFile
+enmFile = sc *> do
+    variants <- many (optional sc *> variantName)
+    pure (EnmFile variants)
+
+variantName :: P Text
+variantName = identifierText
 
 contractIn :: P ContractDecl
 contractIn = do
